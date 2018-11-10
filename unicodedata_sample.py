83c7fa95bb718fe1b69046f1b1a5f6b787eafc16
leeseoungik <leeseoungik@gmail.com>
2018-11-10 16:30:39 +0900

    Update unicodedata_sample.py

--- unicodedata_sample.py
+++ unicodedata_sample.py
@@ -31,13 +31,17 @@ print(unicodedata.name('갏'))
 ##############################
 #unicode문자열 정규화
 
-#NFC는 완성형으로 정규화
-unicodedata.normalize('NFC', '한글')   #정준 분해한 뒤에 다시 정준 결합
-unicodedata.normalize('NFKC', '한글')  #호환 분해한 뒤에 다시 정준 결합
+#완성형으로 정규화
+#정준 분해한 뒤에 다시 정준 결합
+unicodedata.normalize('NFC', '한글')
+#호환 분해한 뒤에 다시 정준 결합
+unicodedata.normalize('NFKC', '한글')
 
-#NFD는 조합형 정규화
-unicodedata.normalize('NFD', '한글')     #정준 분해
-unicodedata.normalize('NFKD', '한글')    #호환 분해
+#N조합형 정규화
+#정준 분해
+unicodedata.normalize('NFD', '한글')
+#호환 분해
+unicodedata.normalize('NFKD', '한글')
 
 #한중일 호환용 한자 유니코드(樂)
 print( unicodedata.normalize('NFC', '\uF914'))
